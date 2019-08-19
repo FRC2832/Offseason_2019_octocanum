16b236a226bb4ff37599f4639a7a5c9c0b604fcc
msnider <msnider@livoniawarriors.org>
2019-08-19 19:33:01 -0400

    Update Drivetrain.java

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,7 +8,10 @@
 package frc.robot.subsystems;
 
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 /**
  * Add your docs here.
@@ -16,8 +19,26 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Drivetrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-      
+  WPI_TalonSRX mfl;
+  WPI_TalonSRX mfr;
+  WPI_TalonSRX mbl;
+  WPI_TalonSRX mbr;
+  MecanumDrive mecanumDrive;
+
+  /*
+  tfl = Tank Front Left
+  tfr = Tank Front Right
+  tbl = Tank Back Left
+  tbr = Tank Back Right
+  tr = Tank Right
+  tl = Tank Left
+  */  
       public Drivetrain(){
+        mfl = new WPI_TalonSRX(0);
+        mfr = new WPI_TalonSRX(1);
+        mbl = new WPI_TalonSRX(2);
+        mbr = new WPI_TalonSRX(3);
+        mecanumDrive = new MecanumDrive(mfl, mbl, mfr, mbr);
 
       }
   
@@ -25,5 +46,9 @@ public class Drivetrain extends Subsystem {
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+  
+  public void setMecanumDriveCommand(double ySpeed, double xSpeed, double zRotation, double gyroAngle){
+    mecanumDrive.driveCartesian(ySpeed, xSpeed, zRotation);
   }
 }
