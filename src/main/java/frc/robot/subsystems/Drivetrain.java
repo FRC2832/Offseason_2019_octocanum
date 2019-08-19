7fb82a1cfebefe6f18a76f5ef743471635ea9fd6
comptsy <comptsy@gmail.com>
2019-08-19 18:59:38 -0400

    Reset to outline for mecanum code

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,11 +7,8 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  * Add your docs here.
@@ -19,29 +16,9 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class Drivetrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-      WPI_TalonSRX tfl;
-      WPI_TalonSRX tfr;
-      WPI_TalonSRX tbl;
-      WPI_TalonSRX tbr;
-      SpeedControllerGroup tr;
-      SpeedControllerGroup tl;
-      public DifferentialDrive tankDrive;
-      /*
-      tfl = Tank Front Left
-      tfr = Tank Front Right
-      tbl = Tank Back Left
-      tbr = Tank Back Right
-      tr = Tank Right
-      tl = Tank Left
-      */
+      
       public Drivetrain(){
-          tfl = new WPI_TalonSRX(0);
-          tfr = new WPI_TalonSRX(1);
-          tbl = new WPI_TalonSRX(2);
-          tbr = new WPI_TalonSRX(3);
-          tr = new SpeedControllerGroup(tfr, tbr);
-          tl = new SpeedControllerGroup(tfl, tbl);
-          tankDrive = new DifferentialDrive(tl, tr);
+
       }
   
   @Override
